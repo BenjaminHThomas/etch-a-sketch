0412cc4d9588b96759e50e81c3b8e5957dd8b96b
Ben <benjamin.h.thomas1@gmail.com>
2023-03-18 18:25:47 +1100

    Optomised 1:
    Declared global variables that are used multiple times rather than in the function.
    
    Added in better comments for the introduction.

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-// Step 1: create a 16 x 16 grid of square divs
+// This function initializes a grid of div elements within a parent div 
+// The size of the grid is determined by the size slider, which defaults to 16.
+// When the user hovers over a div, it changes to a random colour.
+
+const parent = document.querySelector(".gridContainer")
+const boxTemplate = '<div class="box" style="flex-grow: 1;"></div>'
 
 function init(size = 16){
-    // initialises the 16 x 16 grid of square divs
-    const parent = document.querySelector(".gridContainer")
+    // initialises the grid of square divs
     const rowDiv = document.createElement("div");
-
-    rowDiv.className = "row";
+    rowDiv.innerHTML = `${boxTemplate.repeat(size)}`
     rowDiv.style.display = "flex";
     rowDiv.style.flexGrow = 1;
-
-    const box = document.createElement("div");
-    box.className = "box";
-    box.style.flexGrow = 1;
-
-    for (var i = 0; i < size; i++){
-        rowDiv.appendChild(box.cloneNode(true));
-    };
 
     for (var i = 0; i < size; i++){
         parent.appendChild(rowDiv.cloneNode(true));
     };
 };
 
-init();
+function update(){
+    parent.addEventListener('mouseover', (event) => {
+        if (event.target.matches('.box')) {
+            const hue = Math.floor(Math.random() * 360); // Generate a random hue value
+            event.target.style.backgroundColor = `hsl(${hue}, 50%, 50%)`; // Set the background color with the random hue value
+        }
+    })};
 
-const update = () => {
-    let box = document.querySelectorAll(".box")
-    box.forEach(box => box.addEventListener('mouseenter', function(){
-        const hue = Math.floor(Math.random() * 360); // Generate a random hue value
-        box.style.backgroundColor = `hsl(${hue}, 50%, 50%)`; // Set the background color with the random hue value
-    }))
-};
+init();
 update();
 
-
 let size = document.getElementById("size")
-size.addEventListener("input", function() {
-    document.querySelector(".gridContainer").innerHTML = '' //wipe all child nodes
+size.addEventListener("change", function() {
+    parent.innerHTML = '' //wipe all child nodes
     init(size = this.value)
     update();
 })
